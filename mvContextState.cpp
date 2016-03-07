483782c3c66d3884759e97691546871504eed7d1
David C. Lonie <david.lonie@kitware.com>
2016-03-07 15:52:52 -0500

    Use higher quality depth peeling settings.
    There were some noticable artifacts due to insufficient peels. Still
    rendering way faster than our target framerate on a debugging build, so
    this should be fine.

--- mvContextState.cpp
+++ mvContextState.cpp
@@ -8,8 +8,8 @@ mvContextState::mvContextState()
   m_widget->GetRenderWindow()->AddRenderer(m_renderer.GetPointer());
 
   m_renderer->SetUseDepthPeeling(1);
-  m_renderer->SetMaximumNumberOfPeels(4);
-  m_renderer->SetOcclusionRatio(0.1);
+  m_renderer->SetMaximumNumberOfPeels(8);
+  m_renderer->SetOcclusionRatio(0.001);
 }
 
 mvContextState::~mvContextState()
