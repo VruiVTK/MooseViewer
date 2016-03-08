866c563e0ecce217548021f787039790a1ecab1b
David C. Lonie <david.lonie@kitware.com>
2016-03-08 14:01:08 -0500

    Add LODs to mvGeometry.

--- mvGeometry.h
+++ mvGeometry.h
@@ -1,7 +1,7 @@
 #ifndef MVGEOMETRY_H
 #define MVGEOMETRY_H
 
-#include "mvAsyncGLObject.h"
+#include "mvLODAsyncGLObject.h"
 
 #include <vtkNew.h>
 #include <vtkSmartPointer.h>
@@ -14,19 +14,10 @@ class vtkPolyDataMapper;
 /**
  * @brief The mvGeometry class renders the dataset as polydata.
  */
-class mvGeometry : public mvAsyncGLObject
+class mvGeometry : public mvLODAsyncGLObject
 {
 public:
-  using Superclass = mvAsyncGLObject;
-
-  struct DataItem : public Superclass::DataItem
-  {
-    DataItem();
-
-    // Rendering pipeline:
-    vtkNew<vtkPolyDataMapper> mapper;
-    vtkNew<vtkActor> actor;
-  };
+  using Superclass = mvLODAsyncGLObject;
 
   enum Representation
     {
@@ -37,21 +28,68 @@ public:
     SurfaceWithEdges
     };
 
+  struct GeometryState : public Superclass::ObjectState
+  {
+    double opacity{1.};
+    Representation representation{Surface};
+    bool visible{true};
+
+    void update(const mvApplicationState &state) override {}
+  };
+
+  // LoRes LOD: ----------------------------------------------------------------
+  // Run vtkCompositeDataGeometryFilter on the reduced dataset:
+  struct LoResDataPipeline : public Superclass::DataPipeline
+  {
+    vtkNew<vtkCompositeDataGeometryFilter> filter;
+
+    // Returns the dataset to use. This is the only difference between the
+    // LoRes and HiRes pipelines, so this should save some duplication.
+    virtual vtkDataObject* input(const mvApplicationState &state) const;
+
+    void configure(const ObjectState &, const mvApplicationState &) override;
+    bool needsUpdate(const ObjectState &objState,
+                     const LODData &result) const override;
+    void execute() override;
+    void exportResult(LODData &result) const override;
+  };
+
+  // HiRes LOD: ----------------------------------------------------------------
+  // Run vtkCompositeDataGeometryFilter on the full dataset:
+  struct HiResDataPipeline : public LoResDataPipeline
+  {
+    vtkDataObject* input(const mvApplicationState &state) const override;
+  };
+
+  // Shared: LODData and RenderPipeline are shared between LoRes and HiRes. ----
+  struct GeometryLODData : public Superclass::LODData
+  {
+    vtkSmartPointer<vtkDataObject> geometry;
+  };
+
+  struct GeometryRenderPipeline : public Superclass::RenderPipeline
+  {
+    vtkNew<vtkPolyDataMapper> mapper;
+    vtkNew<vtkActor> actor;
+
+    void init(const ObjectState &objState,
+              mvContextState &contextState) override;
+    void update(const ObjectState &objState,
+                const mvApplicationState &appState,
+                const mvContextState &contextState,
+                const LODData &result) override;
+    void disable();
+  };
+
+  // mvGeometry API ------------------------------------------------------------
   mvGeometry();
   ~mvGeometry();
-
-  // mvGLObjectAPI:
-  void initMvContext(mvContextState &mvContext,
-                     GLContextData &contextData) const override;
-  void syncContextState(const mvApplicationState &appState,
-                        const mvContextState &contextState,
-                        GLContextData &contextData) const override;
 
   /**
    * Toggle visibility of the contour props on/off.
    */
-  bool visible() const { return m_visible; }
-  void setVisible(bool visible) { m_visible = visible; }
+  bool visible() const { return this->objectState<GeometryState>().visible; }
+  void setVisible(bool v) { this->objectState<GeometryState>().visible = v; }
 
   /**
    * The actor's opacity.
@@ -66,28 +104,17 @@ public:
   void setRepresentation(Representation representation);
 
 private: // mvAsyncGLObject virtual API:
-  void configureDataPipeline(const mvApplicationState &state) override;
-  bool dataPipelineNeedsUpdate() const override;
-  void executeDataPipeline() const override;
-  void retrieveDataPipelineResult() override;
-  std::string progressLabel() const override { return "Updating Geometry"; }
+  std::string progressLabel() const override { return "Geometry"; }
+
+  ObjectState* createObjectState() const override;
+  DataPipeline* createDataPipeline(LevelOfDetail lod) const override;
+  RenderPipeline* createRenderPipeline(LevelOfDetail lod) const override;
+  LODData* createLODData(LevelOfDetail lod) const override;
 
 private:
   // Not implemented -- disable copy:
   mvGeometry(const mvGeometry&);
   mvGeometry& operator=(const mvGeometry&);
-
-private:
-  // Data pipeline:
-  vtkNew<vtkCompositeDataGeometryFilter> m_filter;
-
-  // Renderable data:
-  vtkSmartPointer<vtkDataObject> m_appData;
-
-  // Render settings
-  double m_opacity;
-  Representation m_representation;
-  bool m_visible;
 };
 
 #endif // MVGEOMETRY_H
